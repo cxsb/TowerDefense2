7a9ea8a40246de39cf2651e9744933039c1c05ec
xi0806 <xi0806@126.com>
2021-04-15 22:53:01 +0800

    家里的无辐射区

---
+++ Assets/Scripts/TowerDefense/RadiationFreeZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A2
+{
+    public class RadiationFreeZone : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            RadiationReciever raditionReciever = other.gameObject.GetComponent<RadiationReciever>();
+            if(raditionReciever != null)
+            {
+                raditionReciever.isInZone = false;
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            RadiationReciever raditionReciever = other.gameObject.GetComponent<RadiationReciever>();
+            if(raditionReciever != null)
+            {
+                raditionReciever.isInZone = true;
+            }
+        }
+    }
+}
