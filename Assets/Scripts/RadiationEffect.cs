a979f63741f4e9be8f85df2b0f6aa60c2e4fff16
xi0806 <xi0806@126.com>
2021-04-16 03:50:45 +0800

    加了辐射的视觉效果

---
+++ Assets/Scripts/RadiationEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A2
+{
+    public class RadiationEffect : MonoBehaviour
+    {
+        public GameObject radiationPanel;
+        public RadiationReciever radiationReciever;
+        // Update is called once per frame
+        void Update()
+        {
+            radiationPanel.SetActive(radiationReciever.isInZone);
+        }
+    }
+}
